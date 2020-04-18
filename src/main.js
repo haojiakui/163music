592947580f1a04e10a50fd6a60c784e62f1caa7f
Your Name <857574577@qq.com>
2020-04-18 14:58:57 +0800

    'API搭建'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/reset.css'
+import './assets/fonts/iconfont.css' //全局导入字体图标
 
 Vue.config.productionTip = false
 
